3ca3c3dd9f3c9232d565ff3082b4ac00b002094b
diana <diana-safina-2017@mail.ru>
2023-09-17 14:03:49 +0500

    исправлено

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -2,6 +2,7 @@ from utils import arrs
 
 
 def test_get():
+    """Исправлено"""
     assert arrs.get([1, 2, 3], 1, "test") == 2
     assert arrs.get([], 0, "test") == "test"
     assert arrs.get([1,2,3], 0, "test") == 1
